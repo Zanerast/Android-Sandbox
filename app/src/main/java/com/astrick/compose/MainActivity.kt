2a7618962f45e42669f48371b5edcfb749a748f6
zaner <zanerast@gmail.com>
2023-06-02 19:32:06 +1200

    Finish dice roller project

--- app/src/main/java/com/astrick/compose/MainActivity.kt
+++ app/src/main/java/com/astrick/compose/MainActivity.kt
@@ -7,13 +7,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.astrick.businesscard.BusinessCard
 import com.astrick.core.ui.theme.BaseComposeTheme
+import com.astrick.diceroller.DiceRoller
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BaseComposeTheme {
-                BusinessCard()
+//                BusinessCard()
+                DiceRoller()
             }
         }
     }
