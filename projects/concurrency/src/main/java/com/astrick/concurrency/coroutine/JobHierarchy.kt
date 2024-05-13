a3327c3827d8dcad0bcca07700fc29383c994ae9
Zane Rastrick <zanerast@gmail.com>
2024-05-13 18:06:17 +1200

    Coroutine jobs

---
+++ projects/concurrency/src/main/java/com/astrick/concurrency/coroutine/JobHierarchy.kt
@@ -0,0 +1,22 @@
+package com.astrick.concurrency.coroutine
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+fun jobHierarchyExample() {
+    val scopeJob = Job()
+    val scope = CoroutineScope(Dispatchers.Default + scopeJob)
+
+    val coroutineJob = scope.launch {
+        println("Starting coroutine")
+        delay(1_000)
+    }
+
+    Thread.sleep(100)
+
+    // True
+    println("Is coroutineJob a child of scopeJob? => ${scopeJob.children.contains(coroutineJob)}")
+}
