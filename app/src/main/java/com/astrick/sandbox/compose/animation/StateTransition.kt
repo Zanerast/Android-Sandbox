b4744fdf326e6b0539722ec9866b30c7181de4ba
Zane Rastrick <zanerast@gmail.com>
2024-12-16 20:42:53 +1300

    Refactor UpdateTransitionSample

--- app/src/main/java/com/astrick/sandbox/compose/animation/StateTransition.kt
+++ app/src/main/java/com/astrick/sandbox/compose/animation/StateTransition.kt
@@ -1,4 +1,4 @@
-package com.astrick.compose.animation
+package com.astrick.sandbox.compose.animation
 
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.animateDp
@@ -9,7 +9,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,14 +21,15 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 
-enum class States {
-    STATE_ONE,
-    STATE_TWO,
-    STATE_THREE
-}
-
+/**
+ * A composable that demonstrates the updateTransition function by toggling between three states,
+ * with each state triggering different animations for color, corner size, and text size.
+ * When the user clicks on the box, the state transitions to the next in sequence.
+ *
+ * @param modifier Modifier to be applied to the Box container.
+ */
 @Composable
-fun StateTransitionSample() {
+fun UpdateTransitionSample(modifier: Modifier = Modifier) {
 
     var state by remember { mutableStateOf(States.STATE_ONE) }
     val transition = updateTransition(
@@ -61,7 +62,7 @@ fun StateTransitionSample() {
         States.STATE_TWO -> "State Two"
         States.STATE_THREE -> "State Three"
     }
-    Box(modifier = Modifier
+    Box(modifier = modifier
         .background(color = color, shape = RoundedCornerShape(cornerSize))
         .clickable {
             state = when(state) {
@@ -77,4 +78,14 @@ fun StateTransitionSample() {
             modifier = Modifier.padding(8.dp)
         )
     }
+}
+
+/**
+ * Enum representing the three possible states for the StateTransitionSample composable.
+ * Each state is associated with different visual properties such as color, corner size, and text size.
+ */
+private enum class States {
+    STATE_ONE,
+    STATE_TWO,
+    STATE_THREE
 }
