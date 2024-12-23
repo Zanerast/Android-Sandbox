a96aa6cf82100ff0ef4ff100b38aa0772ea1341c
Zane Rastrick <zanerast@gmail.com>
2024-12-23 21:52:19 +1300

    Refactor DeeplinkNavigation

--- app/src/main/java/com/astrick/sandbox/compose/navigation/deeplink/DeeplinkNavigation.kt
+++ app/src/main/java/com/astrick/sandbox/compose/navigation/deeplink/DeeplinkNavigation.kt
@@ -1,9 +1,9 @@
-package com.astrick.compose.navigation
+package com.astrick.sandbox.compose.navigation.deeplink
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -13,45 +13,27 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navDeepLink
 
-private const val DEEPLINK_HOME = "destination_home"
 private const val DEEPLINK_GREETING = "destination_greeting"
 
 /**
- * Remember to also add to the manifest
- *
  * Deeplink can be tested with:
- * adb shell am start -W -a android.intent.action.VIEW -d "mydeeplinkapp://greeting/zane"
+ * adb shell am start -a android.intent.action.VIEW -d "deeplink://greeting/zane"
  *
  * adb = Android Debug Bridge
  * shell = tells it we want to execute a shell(text based interface) command
  * am = Activity Manager
  * start = sub-command of am that allows us to start the activity
- * -W = wait for activity to finish before displaying command prompt again (so not actually needed)
  * -d = deeplink
  * -a android.intent.action.VIEW = sets the intent action to use
  * - Note: This isn't required if we have the DEFAULT category set on the intent
- *
- * Can also use this in an html file:
- * <!DOCTYPE html>
- *     <html>
- *         <head>
- *             <title>Deep Link</title>
- *         </head>
- *         <body>
- *             <a href="mydeeplinkapp://greeting/zane">Click here</a>
- *         </body>
- *     </html>
  */
 @Composable
-fun NavWithDeeplink() {
+fun DeeplinkNavigation() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = DEEPLINK_HOME) {
-        composable(DEEPLINK_HOME) {
-            DeepLinkHomeScreen()
-        }
+    NavHost(navController = navController, startDestination = DEEPLINK_GREETING) {
         composable(
             route = DEEPLINK_GREETING,
-            deepLinks = listOf(navDeepLink { uriPattern = "mydeeplinkapp://greeting/{name}" })
+            deepLinks = listOf(navDeepLink { uriPattern = "deeplink://greeting/{name}" })
         ) {
             val name = it.arguments?.getString("name") ?: ""
             DeepLinkGreetingScreen(name)
@@ -60,25 +42,18 @@ fun NavWithDeeplink() {
 }
 
 @Composable
-fun DeepLinkHomeScreen(modifier: Modifier = Modifier) {
-    Box(modifier = modifier.fillMaxSize()) {
-        Text(
-            text = "Welcome to Home screen",
-            fontSize = 30.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth()
-        )
-    }
-}
-
-@Composable
-fun DeepLinkGreetingScreen(
+private fun DeepLinkGreetingScreen(
     name: String,
     modifier: Modifier = Modifier
 ) {
     Box(modifier = modifier.fillMaxSize()) {
+        val message = if (name.isEmpty()) {
+            "Looks like the deeplink failed"
+        } else {
+            "Hello $name. Congrats on the deeplink"
+        }
         Text(
-            text = "Hello $name",
+            text = message,
             fontSize = 30.sp,
             textAlign = TextAlign.Center,
             modifier = Modifier.fillMaxWidth()
