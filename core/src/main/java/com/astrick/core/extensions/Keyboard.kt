4c2361573f747b235a7c1de07da3f94e42b867ec
Zane Rastrick <zanerast@gmail.com>
2024-12-24 21:41:01 +1300

    Refactor LifecycleOwnerExt

--- core/src/main/java/com/astrick/core/extensions/Keyboard.kt
+++
@@ -1,8 +0,0 @@
-package com.astrick.core.extensions
-
-import android.app.Activity
-import android.view.WindowManager
-
-fun Activity.preventKeyboardFromPushingUpUi() {
-    window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING)
-}
