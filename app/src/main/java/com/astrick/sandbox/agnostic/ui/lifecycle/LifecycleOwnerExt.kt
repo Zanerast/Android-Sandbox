960a551ff86e59e45b672a176cd4c27ef4afe8e6
Zane Rastrick <zanerast@gmail.com>
2024-12-24 22:05:38 +1300

    Refactor LifecycleOwnerExt

--- app/src/main/java/com/astrick/sandbox/agnostic/ui/lifecycle/LifecycleOwnerExt.kt
+++ app/src/main/java/com/astrick/sandbox/agnostic/ui/lifecycle/LifecycleOwnerExt.kt
@@ -11,20 +11,6 @@ object LifecycleOwnerExt {
 
     /**
      * Launches a coroutine that runs the provided [block] every time the LifecycleOwner's lifecycle
-     * is in the STARTED state. The coroutine is automatically canceled when the lifecycle state changes.
-     *
-     * @param block The suspend function to execute while in the STARTED state.
-     */
-    inline fun LifecycleOwner.repeatOnStarted(crossinline block: suspend () -> Unit) {
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                block()
-            }
-        }
-    }
-
-    /**
-     * Launches a coroutine that runs the provided [block] every time the LifecycleOwner's lifecycle
      * is in the CREATED state. The coroutine is automatically canceled when the lifecycle state changes.
      *
      * @param block The suspend function to execute while in the CREATED state.
@@ -46,6 +32,20 @@ object LifecycleOwnerExt {
     inline fun LifecycleOwner.repeatOnResume(crossinline block: suspend () -> Unit) {
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                block()
+            }
+        }
+    }
+
+    /**
+     * Launches a coroutine that runs the provided [block] every time the LifecycleOwner's lifecycle
+     * is in the STARTED state. The coroutine is automatically canceled when the lifecycle state changes.
+     *
+     * @param block The suspend function to execute while in the STARTED state.
+     */
+    inline fun LifecycleOwner.repeatOnStarted(crossinline block: suspend () -> Unit) {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
                 block()
             }
         }
