71353b69ec36a2faeca74d6ba1e32971e26671a5
Zane Rastrick <zanerast@gmail.com>
2024-12-26 11:04:16 +1300

    Refactor Channels

---
+++ app/src/main/java/com/astrick/sandbox/kotlin/concurrency/Channel.kt
@@ -0,0 +1,111 @@
+package com.astrick.sandbox.kotlin.concurrency
+
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+/**
+ * Demonstrates the use of a `Channel` for communication between coroutines.
+ *
+ * When to use channels over State/Shared flow:
+ * - When the order of events matters and events should not be overwritten (e.g., queue-like behavior)
+ * - TODO Find an actual production use-case for channels
+ *
+ * References:
+ * [Intro to Coroutines and Channels](www.youtube.com/watch?v=HpWQUoVURWQ&ab_channel=JetBrains)
+ */
+fun channelExample() = runBlocking {
+    val channel = Channel<String>()
+
+    // Producer coroutines
+    launch {
+        channel.send("A1")
+        channel.send("A2")
+        println("A done")
+    }
+    launch {
+        channel.send("B1")
+        channel.send("B2")
+        println("B done")
+    }
+
+    // Consumer coroutine
+    launch {
+        repeat(4) {
+            val x = channel.receive()
+            println("Received: $x")
+        }
+    }
+
+    /*
+      Will print:
+      A1
+      B1
+      A done
+      B done
+      A2
+     */
+
+}
+
+private fun main() {
+    println("Channel Example")
+    channelExample()
+    println("--------------")
+    println("StateFlow Example")
+    stateFlowExample()
+}
+
+/*
+ * Here to demonstrate the use of `StateFlow` vs 'Channel'.
+ *
+ * Differences between `StateFlow` and `Channel`:
+ * 1. **State Retention**:
+ *    - `StateFlow`: Always holds the latest value. Late collectors receive the most recent value only.
+ *    - `Channel`: Can buffer or queue multiple values, ensuring all sent values are processed.
+ *
+ * 2. **Use Case**:
+ *    - `StateFlow`: Best for representing a state that changes over time.
+ *    - `Channel`: Suitable for event-driven communication where every event is important.
+ *
+ * 3. **Concurrency**:
+ *    - `StateFlow`: Thread-safe and supports atomic updates using `update`.
+ *    - `Channel`: Requires careful management of lifecycle and cancellation to avoid resource leaks.
+ *
+ * 4. **Collection**:
+ *    - `StateFlow`: Always active; values are retained until collected.
+ *    - `Channel`: Can suspend senders if there's no active collector.
+ *
+ */
+private fun stateFlowExample() = runBlocking {
+    val stateFlow = MutableStateFlow("Initial Stateflow")
+
+    // Producer coroutines
+    launch {
+        stateFlow.update { "C1" }
+        stateFlow.update { "C2" }
+        println("C done")
+    }
+    launch {
+        stateFlow.update { "D1" }
+        stateFlow.update { "D2" }
+        println("D done")
+    }
+
+    // Collector coroutine
+    launch {
+        stateFlow.collect { value ->
+            println("Received: $value")
+        }
+    }
+
+    /*
+       Will print:
+       C done
+       D done
+       Received: D2
+     */
+
+}
---
+++ app/src/main/java/com/astrick/sandbox/kotlin/concurrency/ChannelActor.kt
@@ -0,0 +1,43 @@
+package com.astrick.sandbox.kotlin.concurrency
+
+import com.astrick.sandbox.kotlin.concurrency.CounterMsg.GetCounter
+import com.astrick.sandbox.kotlin.concurrency.CounterMsg.IncCounter
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.ObsoleteCoroutinesApi
+import kotlinx.coroutines.channels.actor
+import kotlinx.coroutines.runBlocking
+
+/**
+ * **Deprecated**: Currently no replacements for actors.
+ *
+ * Demonstrates the usage of an actor in Kotlin coroutines.
+ *
+ * References:
+ * - [Android-World article](https://medium.com/p/e25f701fa213)
+ * - [Coroutines Issue 87](https://github.com/Kotlin/kotlinx.coroutines/issues/87)
+ * - [KotlinConf 2017 ](https://youtu.be/YrrUCSi72E8?t=2626)
+ * - [Official Docs](https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines.channels/actor.html)
+ */
+@Suppress("unused")
+@OptIn(ObsoleteCoroutinesApi::class)
+fun channelActorExample() = runBlocking {
+    val counterActor = actor<CounterMsg> {
+        var counter = 0
+        for (msg in channel) {
+            when (msg) {
+                is IncCounter -> counter++
+                is GetCounter -> msg.response.complete(counter)
+            }
+        }
+    }
+
+    counterActor.send(IncCounter)
+    val response = CompletableDeferred<Int>()
+    counterActor.send(GetCounter(response = response))
+    println("Counter: ${response.await()}")  // Output will be "Counter: 1"
+}
+
+private sealed interface CounterMsg {
+    object IncCounter : CounterMsg
+    class GetCounter(val response: CompletableDeferred<Int>) : CounterMsg
+}
