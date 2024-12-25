baf6c9dca016aa2e693a1639c5815c8cbdc377fa
Zane Rastrick <zanerast@gmail.com>
2024-12-26 09:43:55 +1300

    Delete a bunch of old stuff

--- projects/kotlin/src/main/java/com/astrick/kotlin/collections/Lists.kt
+++
@@ -1,7 +0,0 @@
-package com.astrick.kotlin.collections
-
-// indexOf needs to start from the start and searches
-// so the more elements the longer it will take
-fun main() {
-    println("Kotlin".hashCode())
-}
--- projects/kotlin/src/main/java/com/astrick/kotlin/collections/Maps.kt
+++
@@ -1,7 +0,0 @@
-package com.astrick.kotlin.collections
-
-// Getting is generally faster than lists
-// But for arrays?
-fun main() {
-
-}
